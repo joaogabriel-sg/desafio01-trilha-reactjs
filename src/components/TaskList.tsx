f2dd4ff9825d7b887d51e70618123d217d4b0fa6
João Gabriel <joaogabriel@pop-os.localdomain>
2021-05-04 11:07:18 -0300

    :tada: challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const randomId = Math.floor(Math.random() * 1000);
+    const newTask = {
+      id: randomId,
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks((oldTasks) => [...oldTasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks((oldTasks) => {
+      const updatedTasks = oldTasks.map((oldTask) => {
+        if (oldTask.id === id) oldTask.isComplete = !oldTask.isComplete;
+        return oldTask;
+      })
+      return updatedTasks;
+    })
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks((oldTasks) => oldTasks.filter((oldTask) => oldTask.id !== id));
   }
 
   return (
